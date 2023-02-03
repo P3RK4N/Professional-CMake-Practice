9e8339953a1b245a8198f5682d957cf1d5562d51
Bruno Perković <perkanperkovic15@gmail.com>
2023-02-03 13:51:53 +0100

    Chapters [4-5]

---
+++ src/MyExe/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Lana je glupa" << std::endl;
+	std::cout << add(2, 3) << std::endl;
+
+	std::cin.get();
+}
--- src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main()
-{
-   std::cout << "Lana je glupa";
-   std::cin.get();
-}
